857f7826f9e6485b4e181781a4cf28acc29449a7
stephecloutier <stephecloutier@gmail.com>
2017-02-09 15:42:15 +0100

    Setup gulp env

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+/* stephe/katbiznis
+ *
+ * /gulpfile.js - Gulp tasks
+ *
+ * coded by stephecloutier@gmail.com
+ * started at 09/02/2017
+ */
+
+ var gulp = require( "gulp" );
+
+// --- Tasks for images
+// --- Tasks for styles
+// --- Tasks for pug
+// --- Tasks for js
+// --- Watch tasks
+// --- Aliases
